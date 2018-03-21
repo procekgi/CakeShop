62c2079f7f40f250d258fec64c70ff33fb9b0f57
procekgi <36749911+procekgi@users.noreply.github.com>
2018-03-20 22:14:00 -0300

    TCC-Contato

---
+++ CakeShopTCC/Models/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeShop.Models
+{
+    public class Contato
+    {
+        public int Id_contato { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Mensagem { get; set; } 
+    }
+}
