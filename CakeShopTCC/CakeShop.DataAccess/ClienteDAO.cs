852e223b124184b2307261f1ccb0270b80d61fda
procekgi <36749911+procekgi@users.noreply.github.com>
2018-03-26 22:05:00 -0300

    PRodutoDAO

--- CakeShopTCC/CakeShop.DataAccess/ClienteDAO.cs
+++ CakeShopTCC/CakeShop.DataAccess/ClienteDAO.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CakeShop.Models;
 
 namespace CakeShop.DataAccess
 {
     public class ClienteDAO
     {
-        //public void Inserir
+        public void Inserir(Cliente obj)
+        {
+            using (SqlConnection conn =
+                new SqlConnection(@"Initial Catalog=CakeShop;
+                            Data Source=localhost;
+                            Integrated Security=SSPI;"))
+            {
+                string strSQL = @"Insert into Cliente(Nome_Cliente, Telefone,Email,Login_usuario, Senha, Endereco, Numero, CEP, Cidade, Estado)
+                                                    @Nome_Cliente,@Telefone,@Email,@Login_usuario, @Senha, @Endereco, @Numero, @CEP, @Cidade, @Estado);";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add("@Nome_Cliente", SqlDbType.VarChar).Value = obj.Nome_Cliente;
+                    cmd.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = obj.Telefone;
+                    cmd.Parameters.Add("@Email", SqlDbType.VarChar).Value = obj.Email;
+                    cmd.Parameters.Add("@Login_usuario", SqlDbType.VarChar).Value = obj.Login_usuario;
+                    cmd.Parameters.Add("@Senha", SqlDbType.VarChar).Value = obj.Senha;
+                    cmd.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = obj.Endereco;
+                    cmd.Parameters.Add("@Numero", SqlDbType.Int).Value = obj.Numero;
+                    cmd.Parameters.Add("@CEP", SqlDbType.Int).Value = obj.CEP;
+                    cmd.Parameters.Add("@Cidade", SqlDbType.VarChar).Value = obj.Cidade;
+                    cmd.Parameters.Add("@Estado", SqlDbType.VarChar).Value = obj.Estado;
+
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
     }
 }
--- CakeShopTCC/CakeShop.DataAccess/ProdutoDAO.cs
+++ CakeShopTCC/CakeShop.DataAccess/ProdutoDAO.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using CakeShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CakeShop.DataAccess
 {
     public class ProdutoDAO
     {
+        public void Inserir(Produto obj)
+        {
+            using (SqlConnection conn =
+                new SqlConnection(@"Initial Catalog=CakeShop;
+                                Data Source=localhost;
+                                Integrated Security=SSPI;"))
+            {
+                string strSQL = @"insert into produto(Nome_Produto, Preco, Id_UnidadeDeMedida,Id_Categoria,Descricao
+                                        values(@Nome_Produto, @Preco, @Id_UnidadeDeMedida,@Id_Categoria, @Descricao);";
+
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add("@Nome_Produto", SqlDbType.VarChar).Value = obj.Nome_Produto;
+                    cmd.Parameters.Add("@Preco", SqlDbType.Decimal).Value = obj.Preco;
+                    cmd.Parameters.Add("@Id_UnidadeDeMedida", SqlDbType.Int).Value = obj.Id_UnidadeDeMedida;
+                    cmd.Parameters.Add("@Id_Categoria", SqlDbType.Int).Value = obj.Id_Categoria;
+                    cmd.Parameters.Add("@Descricao", SqlDbType.VarChar).Value = obj.Descricao;
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+     /*  public List<Produto> BuscarTodos()
+        {
+            var lst = new List<Produto>();
+            using (SqlConnection conn =
+                new SqlConnection(@"Initial Catalog=CakeShop;
+                            Data Source=localhost;
+                            Integrated Security=SSPI;"))
+            {
+                string strSQL = @"select u.Nome, c.Nome, p.Nome_Produto, p.Preco, p.Descricao from Produto p
+                                inner join in UnidadeDeMedida u on (p.Id_UnidadeDeMedida = u.Id)
+                                inner join in Categoria c on (p.Id_Categoria = c.Id);";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    conn.Open();
+                    cmd.CommandText = strSQL;
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+                    conn.Close();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var produto = new Produto()
+                        {
+                            Id_Produto = Convert.ToInt32(row["id"])
+                        };
+                    }
+                }
+            }
+        }*/
     }
 }
