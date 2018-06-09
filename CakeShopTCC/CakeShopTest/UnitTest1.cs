0fa4feedabfabd5fd22ca5f9066c6ea8c0fd546d
procekgi <procekgi@gmail.com>
2018-06-08 22:20:28 -0300

    teste

---
+++ CakeShopTCC/CakeShopTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CakeShop.Models;
+using CakeShop.DataAccess;
+namespace CakeShopTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Confeiteira c = new Confeiteira();
+            c.Email = "procekgi@gmail.com";
+            c.Nome = "Giovana Machado";
+            c.Login = "ConfeiteiraLog";
+            c.Senha = "cs_giovana";
+                                     
+                
+        }
+       
+               
+    }
+}
