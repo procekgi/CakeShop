206e9f059c32fc8912c799ffa3a83b2175ff31a6
procekgi <procekgi@gmail.com>
2018-06-26 21:43:38 -0300

    validaçao de campo vazio
    página cadastro de produto

--- CakeShopTCC/Models/Produto.cs
+++ CakeShopTCC/Models/Produto.cs
@@ -6,12 +6,21 @@ namespace CakeShop.Models
     public class Produto
     {
         public int Id_Produto { get; set; }
-        
+        [Required(ErrorMessage = "Campo Nome do produto é obrigatório") ]
         public string Nome_Produto { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome do produto é obrigatório")]
         public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome do produto é obrigatório")]
         public UnidadeDeMedida UnidadeDeMedida { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome do produto é obrigatório")]
         public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome do produto é obrigatório")]
         public string Descricao { get; set; }
+
         public string Foto { get; set; }
     }
 }
